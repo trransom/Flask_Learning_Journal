02d38fa1c656c6d325414eab33de2c50b917f8fb
Thomas Ransom <32624861+trransom@users.noreply.github.com>
2018-07-05 07:34:58 -0400

    Added fields to Entry Model

--- models.py
+++ models.py
@@ -1,10 +1,16 @@
+import datetime
+
 from peewee import *
 from flask.ext.login import UserMixin
 
 DATABASE = SqliteDatabase('journal.db')
 
 class Entry(UserMixin, Model):
-	
+	title = CharField(max=100)
+	date = DateTimeField(default=datetime.datetime.now)
+	timespent = BigIntegerField()
+	learned = TextField()
+	resources = TextField()
 	
 	class Meta:
 		database = DATABASE
